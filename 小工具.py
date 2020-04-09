41413dafb2d49b462c5be2e781b4c59c53c91dd3
guishoushi126@126.com <guishoushi126@126.com>
2020-04-09 09:14:14 +0800

    布局方式修改成了grid网格式布局！2020年4月9日09:14:11

--- 小工具.py
+++ 小工具.py
@@ -29,21 +29,21 @@ def init_tk():
 
     photo_excel = tk.PhotoImage(file="Excel.png")  # file：t图片路径
     excel = tk.Button(text="Excel", image=photo_excel, command=close_execl, bd=5)
-    excel.place(x=0, y=0)
+    excel.grid()
 
     photo_word = tk.PhotoImage(file="word.png")  # file：t图片路径
     word = tk.Button(text="word", image=photo_word, command=close_word, bd=5)
-    word.place(x=60, y=0)
+    word.grid(row=0, column=1)
 
     photo_popint = tk.PhotoImage(file="popint.png")  # file：t图片路径
     popint = tk.Button(text="popint", image=photo_popint, bd=5, command=close_popint)
-    popint.place(x=120, y=0)
+    popint.grid(row=0, column=2)
     w.mainloop()
 
 
 def main():
     init_tk()
 
+
 if __name__ == "__main__":
     main()
-
